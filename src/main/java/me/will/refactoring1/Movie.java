7632fca13ce2abfbb860bec5c6dab30b984e6c58
Will <yzj_000@163.com>
2024-02-28 21:51:22 +0800

    Step 2: rename a few local variables

--- src/main/java/me/will/refactoring1/Movie.java
+++ src/main/java/me/will/refactoring1/Movie.java
@@ -95,25 +95,25 @@ class Customer {
         return result;
     }
 
-    private static double amountFor(Rental each) {
-        double thisAmount = 0;
-        switch (each.getMovie().getPriceCode()) {
+    private static double amountFor(Rental aRental) {
+        double result = 0;
+        switch (aRental.getMovie().getPriceCode()) {
             case Movie.REGULAR -> {
-                thisAmount += 2;
-                if (each.getDaysRented() > 2) {
-                    thisAmount += (each.getDaysRented() - 2) * 1.5;
+                result += 2;
+                if (aRental.getDaysRented() > 2) {
+                    result += (aRental.getDaysRented() - 2) * 1.5;
                 }
             }
 
-            case Movie.NEW_RELEASE -> thisAmount += each.getDaysRented() * 3;
+            case Movie.NEW_RELEASE -> result += aRental.getDaysRented() * 3;
 
             case Movie.CHILDRENS -> {
-                thisAmount += 1.5;
-                if (each.getDaysRented() > 3) {
-                    thisAmount += (each.getDaysRented() - 3) * 1.5;
+                result += 1.5;
+                if (aRental.getDaysRented() > 3) {
+                    result += (aRental.getDaysRented() - 3) * 1.5;
                 }
             }
         }
-        return thisAmount;
+        return result;
     }
 }
