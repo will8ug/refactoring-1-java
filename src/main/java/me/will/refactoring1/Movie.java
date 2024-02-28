1d00f6321c9a3ec45f72991eed7967392b1d0946
Will <yzj_000@163.com>
2024-02-28 23:40:02 +0800

    Remove an unnecessary call of String.valueOf()

--- src/main/java/me/will/refactoring1/Movie.java
+++ src/main/java/me/will/refactoring1/Movie.java
@@ -107,13 +107,13 @@ class Customer {
             }
 
             // show figures for this rental
-            result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
+            result += "\t" + each.getMovie().getTitle() + "\t" + thisAmount + "\n";
             totalAmount += thisAmount;
         }
 
         // add footer lines
-        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-        result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+        result += "Amount owed is " + totalAmount + "\n";
+        result += "You earned " + frequentRenterPoints + " frequent renter points";
         return result;
     }
 
