1e386a0e5b5cd65a36923e501f3bcd23d85f74d4
Will <yzj_000@163.com>
2024-03-03 15:30:21 +0800

    Step 9: move getCharge() from Movie to Price

--- src/main/java/me/will/refactoring1/Movie.java
+++ src/main/java/me/will/refactoring1/Movie.java
@@ -34,25 +34,7 @@ public class Movie {
     }
 
     public double getCharge(int daysRented) {
-        double result = 0;
-        switch (getPriceCode()) {
-            case Movie.REGULAR -> {
-                result += 2;
-                if (daysRented > 2) {
-                    result += (daysRented - 2) * 1.5;
-                }
-            }
-
-            case Movie.NEW_RELEASE -> result += daysRented * 3;
-
-            case Movie.CHILDRENS -> {
-                result += 1.5;
-                if (daysRented > 3) {
-                    result += (daysRented - 3) * 1.5;
-                }
-            }
-        }
-        return result;
+        return _price.getCharge(daysRented);
     }
 
     public int getFrequentRenterPoints(int daysRented) {
@@ -154,6 +136,28 @@ class Customer {
 
 abstract class Price {
     abstract int getPriceCode();
+
+    public double getCharge(int daysRented) {
+        double result = 0;
+        switch (getPriceCode()) {
+            case Movie.REGULAR -> {
+                result += 2;
+                if (daysRented > 2) {
+                    result += (daysRented - 2) * 1.5;
+                }
+            }
+
+            case Movie.NEW_RELEASE -> result += daysRented * 3;
+
+            case Movie.CHILDRENS -> {
+                result += 1.5;
+                if (daysRented > 3) {
+                    result += (daysRented - 3) * 1.5;
+                }
+            }
+        }
+        return result;
+    }
 }
 
 class ChildrensPrice extends Price {
